338b38f08ed5032a93e07d37368891904ee49688
mdd255 <mducdung255@gmail.com>
2025-07-31 03:03:22 +0700

    feat(flash): enhance plugin by AI

--- Code/vscode-nvim/lua/plugins/flash.lua
+++ Code/vscode-nvim/lua/plugins/flash.lua
@@ -1,7 +1,12 @@
 return {
-	'folke/flash.nvim',
+	"folke/flash.nvim",
+	config = function(_, opts)
+		local utils = require('base.utils')
+		utils.hi("FlashLabel", { fg = utils.color.red, bg = "none" })
+		require('flash').setup(opts)
+	end,
 	opts = {
-		labels = 'neioharstdqwfpluy;zxcvkm',
+		labels = "neioharstdqwfpluy;zxcvkm-",
 		modes = {
 			char = {
 				keys = {}
@@ -9,62 +14,63 @@ return {
 		},
 		label = {
 			uppercase = false,
-			current = true,
+			current = false,
 			before = true,
 			after = false,
-		}
+		},
 	},
 	keys = {
 		{
-			'W',
-			mode = { 'n', 'v', 'o' },
+			"W",
+			mode = { "n", "v", "o" },
 			function()
-				require('flash').jump({
+				require("flash").jump({
 					multi_windows = false,
 					search = { mode = "search", max_length = 0 },
 					pattern = [[\<]],
 				})
 			end,
-			desc = 'Flash to beginning of word'
+			desc = "Flash to beginning of word",
 		},
 		{
-			'B',
-			mode = { 'n', 'v', 'o' },
+			"B",
+			mode = { "n", "v", "o" },
 			function()
-				require('flash').jump({
+				require("flash").jump({
 					multi_windows = false,
 					search = { mode = "search", max_length = 0 },
 					pattern = [[\>]],
 				})
 			end,
-			desc = 'Flash to end of word'
+			desc = "Flash to end of word",
+		},
+		{
+			"<C-t>",
+			mode = { "n", "v", "o" },
+			function()
+				require("flash").jump({ multi_windows = false })
+			end,
+			desc = "Flash to any",
+		},
+		{
+			"<C-c>",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump({ pattern = vim.fn.expand("<cword>") })
+			end,
+			desc = "Flash current word",
 		},
 		{
 			"'",
-			mode = { 'n', 'v', 'o' },
+			mode = { "n", "x", "o" },
 			function()
-				require('flash').jump({ multi_windows = false })
-			end,
-			desc = 'Flash to any'
-		},
-		{
-			'<C-c>',
-			mode = { 'n', 'x', 'o' },
-			function()
-				require('flash').jump({ pattern = vim.fn.expand('<cword>') })
-			end,
-			desc = 'Flash current word'
-		},
-		{
-			'<C-t>',
-			mode = { 'n', 'x', 'o' },
-			function()
-				require('flash').jump({
-					pattern = "[(\\[{<]", -- matches all open brackets: (, [, {, <
+				require("flash").jump({
+					pattern = [=[[\[({<]\+]=],
+					search = { mode = "search" },
 					label = { after = false, before = true },
 				})
 			end,
-			desc = 'Flash open bracket'
+			desc = "Flash open bracket/quote",
 		},
 	},
 }
