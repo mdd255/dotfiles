871a2e735b58c2e4ac66a1038cb5b735b8633a91
mdd255 <mducdung255@gmail.com>
2025-08-04 08:26:17 +0700

    feat: automate commit

--- Code/vscode-nvim/lua/plugins/flash.lua
+++ Code/vscode-nvim/lua/plugins/flash.lua
@@ -1,76 +1,30 @@
 return {
 	"folke/flash.nvim",
 	config = function(_, opts)
-		local utils = require('base.utils')
+		local utils = require("base.utils")
 		utils.hi("FlashLabel", { fg = utils.color.red, bg = "none" })
-		utils.hi("FlashLabel2", { fg = utils.color.white, bg = "none" })
-		utils.hi("FlashMatch", { bg = "none" })
-		require('flash').setup(opts)
+		utils.hi("FlashMatch", { fg = utils.color.gray, bg = "none" })
+		utils.hi("FlashBackdrop", { fg = utils.color.gray, bg = "none" })
+		utils.hi("FlashCurrent", { fg = utils.color.blue, bg = "none" })
+		require("flash").setup(opts)
 	end,
 	opts = {
-		labels = "neioharstdqwfpluy;zxcvkm",
+		labels = "neioharstdqwfpluyzxcvkmNEIOHARSTDQWFPLUYZXCVKM1234567890@#$%^&*()_+",
 		modes = {
 			char = {
-				keys = {}
-			}
+				keys = {},
+			},
 		},
 		label = {
 			uppercase = false,
 			current = false,
 			before = true,
 			after = false,
-			format = function(opts)
-				if opts.match.label1 and opts.match.label2 then
-					return {
-						{ opts.match.label1, "FlashLabel" },
-						{ opts.match.label2, "FlashLabel2" },
-					}
-				end
-
-				return { { opts.match.label or "", "FlashLabel" } }
-			end,
 		},
-		labeler = function(matches, state)
-			local labels = state:labels()
-			local label_count = #labels
-
-			for m, match in ipairs(matches) do
-				if m <= label_count then
-					match.label = labels[m]
-				else
-					match.label1 = labels[math.floor((m - 1 - label_count) / label_count) + 1]
-					match.label2 = labels[(m - 1 - label_count) % label_count + 1]
-					match.label = match.label1
-				end
-			end
-		end,
-		-- action = function(match, state)
-		-- if match.label1 and match.label2 then
-		-- state:hide()
-		-- require("flash").jump({
-		-- search = { max_length = 0 },
-		-- highlight = { matches = false },
-		-- matcher = function(win)
-		-- return vim.tbl_filter(function(m)
-		-- return m.label1 == match.label1 and m.win == win
-		-- end, state.results)
-		-- end,
-		-- labeler = function(matches)
-		-- for _, m in ipairs(matches) do
-		-- m.label = m.label2
-		-- m.label1 = nil
-		-- m.label2 = nil
-		-- end
-		-- end,
-		-- })
-		-- else
-		-- state:jump(match)
-		-- end
-		-- end,
 	},
 	keys = {
 		{
-			"W",
+			"<tab>",
 			mode = { "n", "v", "o" },
 			function()
 				require("flash").jump({
@@ -82,7 +36,7 @@ return {
 			desc = "Flash to beginning of word",
 		},
 		{
-			"B",
+			"<s-tab>",
 			mode = { "n", "v", "o" },
 			function()
 				require("flash").jump({
@@ -94,32 +48,28 @@ return {
 			desc = "Flash to end of word",
 		},
 		{
-			"<C-t>",
-			mode = { "n", "v", "o" },
-			function()
-				require("flash").jump({ multi_windows = false })
-			end,
-			desc = "Flash to any",
-		},
-		{
-			"<C-c>",
-			mode = { "n", "x", "o" },
-			function()
-				require("flash").jump({ pattern = vim.fn.expand("<cword>") })
-			end,
-			desc = "Flash current word",
-		},
-		{
 			"'",
 			mode = { "n", "x", "o" },
 			function()
 				require("flash").jump({
-					pattern = [=[[\[({<]\+]=],
+					pattern = [=[[([{<]]=],
 					search = { mode = "search" },
-					label = { after = false, before = true },
+					label = { after = false, before = true, current = false },
 				})
 			end,
-			desc = "Flash open bracket/quote",
+			desc = "Flash open bracket",
+		},
+		{
+			'"',
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump({
+					pattern = [=[['"`]]=],
+					search = { mode = "search" },
+					label = { after = false, before = true, current = false },
+				})
+			end,
+			desc = "Flash quotes/backtick",
 		},
 	},
 }
