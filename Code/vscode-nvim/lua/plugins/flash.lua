1f5e1c61ac165ff0a7e2f247ffae7f6a9c943c8c
mdd255 <mducdung255@gmail.com>
2025-08-05 11:43:21 +0700

    feat(flash): add text object actions

--- Code/vscode-nvim/lua/plugins/flash.lua
+++ Code/vscode-nvim/lua/plugins/flash.lua
@@ -22,7 +22,46 @@ return {
 			after = false,
 		},
 	},
-	keys = {
+	keys = function()
+		local function create_text_object_action(op, text_obj)
+			return function(match, state)
+				local original_win = vim.api.nvim_get_current_win()
+				local original_pos = vim.api.nvim_win_get_cursor(original_win)
+
+				vim.api.nvim_win_call(match.win, function()
+					vim.api.nvim_win_set_cursor(match.win, match.pos)
+					local line = vim.fn.getline(".")
+					local col = vim.fn.col(".")
+					local char = line:sub(col, col)
+
+					local motion_map = {
+						["("] = op .. text_obj .. ")",
+						["["] = op .. text_obj .. "]",
+						["{"] = op .. text_obj .. "}",
+						["<"] = op .. text_obj .. ">",
+						['"'] = op .. text_obj .. '"',
+						["'"] = op .. text_obj .. "'",
+						["`"] = op .. text_obj .. "`",
+					}
+
+					local motion = motion_map[char] or (op .. text_obj .. "w")
+					vim.cmd("normal! " .. motion)
+
+					-- For change operations, enter insert mode
+					if op == "c" then
+						vim.cmd("startinsert")
+					end
+				end)
+
+				-- For delete/yank operations, return to original position
+				if op == "d" or op == "y" then
+					vim.api.nvim_set_current_win(original_win)
+					vim.api.nvim_win_set_cursor(original_win, original_pos)
+				end
+			end
+		end
+
+		return {
 		{
 			"<tab>",
 			mode = { "n", "v", "o" },
@@ -67,27 +106,7 @@ return {
 					pattern = [=[[([{<"'`]]=],
 					search = { mode = "search" },
 					label = { after = false, before = true, current = false },
-					action = function(match, state)
-						vim.api.nvim_win_call(match.win, function()
-							vim.api.nvim_win_set_cursor(match.win, match.pos)
-							local line = vim.fn.getline(".")
-							local col = vim.fn.col(".")
-							local char = line:sub(col, col)
-
-							local motion_map = {
-								["("] = "di)",
-								["["] = "di]",
-								["{"] = "di}",
-								["<"] = "di>",
-								['"'] = 'di"',
-								["'"] = "di'",
-								["`"] = "di`",
-							}
-
-							local motion = motion_map[char] or "diw"
-							vim.cmd("normal! " .. motion)
-						end)
-					end,
+						action = create_text_object_action("d", "i"),
 				})
 			end,
 			desc = "Flash delete inside",
@@ -100,30 +119,63 @@ return {
 					pattern = [=[[([{<"'`]]=],
 					search = { mode = "search" },
 					label = { after = false, before = true, current = false },
-					action = function(match, state)
-						vim.api.nvim_win_call(match.win, function()
-							vim.api.nvim_win_set_cursor(match.win, match.pos)
-							local line = vim.fn.getline(".")
-							local col = vim.fn.col(".")
-							local char = line:sub(col, col)
-
-							local motion_map = {
-								["("] = "da)",
-								["["] = "da]",
-								["{"] = "da}",
-								["<"] = "da>",
-								['"'] = 'da"',
-								["'"] = "da'",
-								["`"] = "da`",
-							}
-
-							local motion = motion_map[char] or "daw"
-							vim.cmd("normal! " .. motion)
-						end)
-					end,
+						action = create_text_object_action("d", "a"),
+					})
+				end,
+				desc = "Flash delete around",
+			},
+			{
+				"cn",
+				mode = "n",
+				function()
+					require("flash").jump({
+						pattern = [=[[([{<"'`]]=],
+						search = { mode = "search" },
+						label = { after = false, before = true, current = false },
+						action = create_text_object_action("c", "i"),
+					})
+				end,
+				desc = "Flash change inside",
+			},
+			{
+				"ce",
+				mode = "n",
+				function()
+					require("flash").jump({
+						pattern = [=[[([{<"'`]]=],
+						search = { mode = "search" },
+						label = { after = false, before = true, current = false },
+						action = create_text_object_action("c", "a"),
 				})
 			end,
-			desc = "Flash delete around",
+				desc = "Flash change around",
 		},
-	},
+			{
+				"yn",
+				mode = "n",
+				function()
+					require("flash").jump({
+						pattern = [=[[([{<"'`]]=],
+						search = { mode = "search" },
+						label = { after = false, before = true, current = false },
+						action = create_text_object_action("y", "i"),
+					})
+				end,
+				desc = "Flash yank inside",
+			},
+			{
+				"ye",
+				mode = "n",
+				function()
+					require("flash").jump({
+						pattern = [=[[([{<"'`]]=],
+						search = { mode = "search" },
+						label = { after = false, before = true, current = false },
+						action = create_text_object_action("y", "a"),
+					})
+				end,
+				desc = "Flash yank around",
+			}
+		}
+	end,
 }
