80f3e25e07be66b95f1c5a96c7fbf2a331534466
mdd255 <mducdung255@gmail.com>
2025-09-19 01:13:17 +0700

    feat(lazyvim): config bufferline

---
+++ lazyvim/lua/config/color.lua
@@ -0,0 +1,16 @@
+local color = {
+  black = "#0a0a0a",
+  cyan = "#2bbac5",
+  yellow = "#e5c07b",
+  orange = "#d19a66",
+  white = "#eeeeee",
+  purple = "#d38aea",
+  blue = "#61afef",
+  red = "#ef596f",
+  light_red = "#00ffff",
+  green = "#89ca78",
+  gray = "#555555",
+  dark_gray = "#252525",
+}
+
+return color
--- lazyvim/lua/plugins/bufferline.lua
+++ lazyvim/lua/plugins/bufferline.lua
@@ -1,3 +1,5 @@
+local color = require("config/color")
+
 return {
   "akinsho/bufferline.nvim",
   optional = true,
@@ -9,9 +11,39 @@ return {
       right_mouse_command = false,
       left_mouse_command = false,
       middle_mouse_command = false,
+      show_buffer_icons = false,
       show_buffer_close_icons = false,
       show_close_icon = false,
       separator_style = "slope",
+      show_duplicate_prefix = false,
+      always_show_bufferline = true,
+    },
+    highlights = {
+      background = {
+        bg = color.black,
+        fg = color.gray,
+      },
+      fill = {
+        bg = color.dark_gray,
+      },
+      separator = {
+        fg = color.dark_gray,
+        bg = color.black,
+      },
+      separator_selected = {
+        fg = color.dark_gray,
+        bg = color.black,
+      },
+      buffer_selected = {
+        bg = color.black,
+        fg = color.blue,
+        bold = true,
+        italic = false,
+      },
+      tab_close = {
+        bg = color.white,
+        fg = color.gray,
+      },
     },
   },
   keys = false,
