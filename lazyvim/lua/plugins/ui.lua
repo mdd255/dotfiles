c75e1a0fc5ae558b270ec30eade52ab113cc16d8
mdd255 <208671806+mdd255@users.noreply.github.com>
2025-09-24 15:42:58 +0700

    feat(lazyvim): update key conflict

--- lazyvim/lua/plugins/ui.lua
+++ lazyvim/lua/plugins/ui.lua
@@ -1,31 +1,30 @@
 return {
   {
-
     "folke/noice.nvim",
     keys = {
       {
         "<c-n>",
         function()
           if not require("noice.lsp").scroll(4) then
-            return "<c-n>"
+            return "<C-n>"
           end
         end,
         silent = true,
         expr = true,
         desc = "Scroll Forward",
-        mode = { "i", "n", "s" },
+        mode = { "n", "s" },
       },
       {
         "<c-e>",
         function()
           if not require("noice.lsp").scroll(-4) then
-            return "<c-e>"
+            return "<C-e>"
           end
         end,
         silent = true,
         expr = true,
         desc = "Scroll Backward",
-        mode = { "i", "n", "s" },
+        mode = { "n", "s" },
       },
     },
     ops = {
