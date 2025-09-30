87ec426e2b3e2d5ea8bc568563bc02368f79b719
mdd255 <mducdung255@gmail.com>
2025-09-30 14:45:16 +0700

    feat(lzvim): integrate neovide

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -8,3 +8,13 @@ vim.opt.mouse = ""
 vim.opt.spell = true
 vim.opt.spelllang = "en_us"
 vim.opt.spellfile = vim.fn.stdpath("config") .. "/spell/index.utf-8.add"
+
+if vim.g.neovide then
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_fullscreen = true
+  vim.g.neovide_position_animation_length = 0.2
+  vim.g.neovide_cursor_animation_length = 0.2
+  vim.g.neovide_cursor_short_animation_length = 0.05
+  vim.g.neovide_cursor_trail_size = 0.5
+  vim.g.neovide_cursor_vfx_mode = "pixiedust"
+end
