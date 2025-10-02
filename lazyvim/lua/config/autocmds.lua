f247a6bab75ec275aee74c759d25812eb979d12e
mdd255 <mducdung255@gmail.com>
2025-10-02 16:46:28 +0700

    feat: automate commit

--- lazyvim/lua/config/autocmds.lua
+++ lazyvim/lua/config/autocmds.lua
@@ -1,16 +1,30 @@
--- autocmds to toggle cursorline
+-- Enable cursorline only if filetype is not in the blacklist
 vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
   callback = function()
-    vim.opt_local.cursorline = true
+    local ft = vim.bo.filetype
+
+    local blacklist = {
+      gitcommit = true,
+      snacks_dashboard = true,
+    }
+
+    vim.opt_local.relativenumber = true
+
+    if not blacklist[ft] then
+      vim.opt_local.cursorline = true
+    end
   end,
 })
 
+-- Always disable on leave
 vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
   callback = function()
     vim.opt_local.cursorline = false
+    vim.opt_local.relativenumber = false
   end,
 })
 
+-- disable
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local client = vim.lsp.get_client_by_id(args.data.client_id)
