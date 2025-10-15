5a13b2c98784061fca1bc842e9774cdf92264460
mdd255 <mducdung255@gmail.com>
2025-10-16 03:35:44 +0700

    feat: add code-companion

--- lazyvim/lua/plugins/code-companion.lua
+++ lazyvim/lua/plugins/code-companion.lua
@@ -28,7 +28,7 @@ return {
           clear = {
             modes = { n = { "tl" } },
           },
-          codeblock = {
+          super_diff = {
             modes = { n = { "t<Cr>" } },
           },
           goto_file_under_cursor = {
@@ -40,8 +40,24 @@ return {
           memory = {
             modes = { n = { "tx" } },
           },
+          next_chat = {
+            modes = { n = { "tn" } },
+          },
+          previous_chat = {
+            modes = { n = { "te" } },
+          },
         },
       },
     },
+  },
+  keys = {
+    { "<leader>aa", "<cmd>CodeCompanionActions<cr>", desc = "CodeCompanion actions", mode = { "n", "v" } },
+    { "<leader>aa", "<cmd>CodeCompanionChat Toggle<cr>", desc = "Toggle CodeCompanion chat" },
+    { "<leader>af", "<cmd>CodeCompanionChat Add<cr>", desc = "Add current file to chat", mode = { "n", "v" } },
+    { "<leader>ai", "<cmd>CodeCompanion<cr>", desc = "Prompt CodeCompanion inline", mode = { "n", "v" } },
+    { "<leader>ar", "<cmd>CodeCompanionActions Review<cr>", desc = "Review selected code", mode = "v" },
+    { "<leader>ae", "<cmd>CodeCompanionActions Explain<cr>", desc = "Explain selected code", mode = "v" },
+    { "<leader>au", "<cmd>CodeCompanionActions UnitTests<cr>", desc = "Generate unit tests", mode = "v" },
+    { "<leader>ad", "<cmd>CodeCompanionActions Debug<cr>", desc = "Debug code", mode = "v" },
   },
 }
