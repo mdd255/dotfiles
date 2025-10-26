be390a40d01e4bf67764f7753ed73c3c1a7cc8a2
mdd255 <mducdung255@gmail.com>
2025-10-26 12:33:21 +0700

    feat: automate commit

--- lazyvim/lua/plugins/blink-cmp.lua
+++ lazyvim/lua/plugins/blink-cmp.lua
@@ -1,18 +1,5 @@
 return {
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-      filetypes = {
-        markdown = true,
-        help = true,
-      },
-    },
-  },
-  {
     "saghen/blink.cmp",
     dependencies = {
       "fang2hou/blink-copilot",
