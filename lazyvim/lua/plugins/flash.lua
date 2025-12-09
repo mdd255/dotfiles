12768a0d32c9c029ff07122374725d2e9de16a3c
mdd255 <mducdung255@gmail.com>
2025-12-09 21:22:51 +0700

    feat(lazyvim): support 2 chars flash word

--- lazyvim/lua/plugins/flash.lua
+++ lazyvim/lua/plugins/flash.lua
@@ -3,7 +3,7 @@ return {
   opts = {
     exclude = { "snacks_dashboard" },
     jump = { autojump = false },
-    labels = "neioharstqfpluyzxkmgj;-NEIOHARSTQFLUYZXVKMBGJ1234567890!@#$%^&*()_+",
+    labels = "neioharstqfpluyzxkmgj;-",
     modes = {
       char = {
         keys = {},
@@ -20,6 +20,59 @@ return {
     },
   },
   keys = function()
+    local Flash = require("flash")
+
+    ---@param opts Flash.Format
+    local function format_first_match(opts)
+      -- always show first and second label
+      return {
+        { opts.match.label1, opts.hl_group },
+        { opts.match.label2, opts.hl_group },
+      }
+    end
+
+    local function format_second_match(opts)
+      return {
+        { opts.match.label2, opts.hl_group },
+      }
+    end
+
+    local function action(match, state)
+      state:hide()
+
+      Flash.jump({
+        search = {
+          max_length = 0,
+        },
+        highlight = { matches = false },
+        label = {
+          after = { 0, 2 },
+          format = format_second_match,
+        },
+        matcher = function(win)
+          -- limit matches to the current label
+          return vim.tbl_filter(function(m)
+            return m.label == match.label and m.win == win
+          end, state.results)
+        end,
+        labeler = function(matches)
+          for _, m in ipairs(matches) do
+            m.label = m.label2 -- use the second label
+          end
+        end,
+      })
+    end
+
+    local function labeler(matches, state)
+      local labels = state:labels()
+
+      for m, match in ipairs(matches) do
+        match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+        match.label2 = labels[(m - 1) % #labels + 1]
+        match.label = match.label1
+      end
+    end
+
     return {
       {
         "<tab>",
@@ -57,15 +110,21 @@ return {
         "w",
         mode = { "n" },
         function()
-          require("flash").jump({
+          Flash.jump({
             search = {
               mode = "search",
-              max_length = 0,
               wrap = false,
               forward = true,
-              multi_windows = false,
+            },
+            label = {
+              after = false,
+              before = { 0, 0 },
+              uppercase = false,
+              format = format_first_match,
             },
             pattern = [[\<]],
+            action = action,
+            labeler = labeler,
           })
         end,
         desc = "Flash forward to beginning of word",
@@ -91,15 +150,21 @@ return {
         "b",
         mode = { "n" },
         function()
-          require("flash").jump({
+          Flash.jump({
             search = {
               mode = "search",
-              max_length = 0,
-              multi_windows = false,
               wrap = false,
               forward = false,
             },
+            label = {
+              after = false,
+              before = { 0, 0 },
+              uppercase = false,
+              format = format_first_match,
+            },
             pattern = [[\<]],
+            action = action,
+            labeler = labeler,
           })
         end,
         desc = "Flash backward to beginning of word",
