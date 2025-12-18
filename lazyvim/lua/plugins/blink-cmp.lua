043e63c1d1e2bf83b18a5948618977059b1eb3a4
mdd255 <mducdung255@gmail.com>
2025-12-18 16:19:31 +0700

    feat: automate commit

--- lazyvim/lua/plugins/blink-cmp.lua
+++ lazyvim/lua/plugins/blink-cmp.lua
@@ -5,20 +5,39 @@ return {
       "kristijanhusak/vim-dadbod-completion",
     },
     opts = {
+      fuzzy = {
+        sorts = {
+          "exact",
+          "score",
+          "label",
+          "sort_text",
+        },
+      },
       keymap = {
         preset = "none",
         ["<Tab>"] = { "insert_next", "snippet_forward", "fallback" },
-        ["<Cr>"] = { "accept", "fallback" },
         ["<S-Tab>"] = { "insert_prev", "snippet_backward", "fallback" },
+        ["<Down>"] = { "insert_next", "fallback" },
+        ["<Up>"] = { "insert_prev", "fallback" },
+        ["<Cr>"] = { "accept", "fallback" },
         ["<C-n>"] = { "scroll_documentation_down", "fallback" },
         ["<C-e>"] = { "scroll_documentation_up", "fallback" },
         ["<C-o>"] = { "hide", "fallback" },
+        ["<C-d>"] = { "show_documentation", "fallback" },
+        ["<C-u>"] = { "hide_documentation", "fallback" },
+        ["<C-i>"] = { "fallback" },
       },
       completion = {
         accept = { auto_brackets = { enabled = true } },
         ghost_text = { enabled = true },
-        menu = { enabled = true, scrollbar = false },
-        documentation = { window = { scrollbar = false } },
+        menu = {
+          enabled = true,
+          scrollbar = false,
+          draw = {
+            snippet_indicator = "󰘖",
+          },
+        },
+        documentation = { window = { scrollbar = false }, auto_show = false },
         list = {
           selection = { preselect = true, auto_insert = true },
           cycle = { from_top = false },
@@ -42,7 +61,7 @@ return {
       cmdline = {
         keymap = {
           preset = "inherit",
-          ["<Cr>"] = { "accept", "fallback" },
+          ["<Cr>"] = { "accept_and_enter", "fallback" },
         },
         completion = {
           menu = { auto_show = true },
