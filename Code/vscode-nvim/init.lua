29465404b447d197baf8581005205bd4971d8ff2
mdd255 <mducdung255@gmail.com>
2026-04-01 08:31:04 +0700

    feat: automate commit

--- Code/vscode-nvim/init.lua
+++ Code/vscode-nvim/init.lua
@@ -1,3 +1,6 @@
+-- When loaded via -u flag, runtimepath doesn't include this config dir
+vim.opt.runtimepath:prepend(vim.fn.fnamemodify(debug.getinfo(1, "S").source:sub(2), ":h"))
+
 _G.event_delay_ms = 100
 require("base")
 require("configs")
--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -16,7 +16,6 @@ vim.opt.mouse = ""
 vim.opt.wrap = false
 vim.g.diffview_active = false
 vim.opt.spell = false
-vim.opt.scroll = 25
 vim.opt.signcolumn = "yes"
 vim.opt.statuscolumn = "%=%{v:relnum==0?v:lnum:v:relnum}%s"
 
