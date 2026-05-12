58535c1c87e0b26be2c47929d85ab057ea520f38
mdd255 <mducdung255@gmail.com>
2026-05-12 09:48:43 +0700

    chore(lazyvim): remove unused plugins

--- lazyvim/lua/plugins/disable.lua
+++ lazyvim/lua/plugins/disable.lua
@@ -7,4 +7,5 @@ return {
   { "folke/todo-comments.nvim", enabled = false },
   { "rafamadriz/friendly-snippets", enabled = false },
   { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
+  { "folke/lazydev.nvim", enabled = false },
 }
